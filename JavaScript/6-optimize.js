0569d27a8fc8c1ca84ded7cd4983ce5510e3fa3e
tshemsedinov <timur.shemsedinov@gmail.com>
2018-09-30 22:42:00 +0300

    Oplimized examples with fp and oop

---
+++ JavaScript/6-optimize.js
@@ -0,0 +1,27 @@
+'use strict';
+
+const view = (lens, obj) => lens.get(obj);
+const set = (lens, val, obj) => lens.set(val, obj);
+const over = (lens, map, obj) => lens.set(map(lens.get(obj)), obj);
+
+const lens = (source, destination = source) => ({
+  get: obj => obj[source],
+  set: (val, obj) => ({ ...obj, [destination]: val })
+});
+
+// Usage
+
+const person = {
+  name: 'Marcus Aurelius',
+  city: 'Rome',
+  born: 121
+};
+
+const nameLens = lens('name');
+
+console.log('view name:', view(nameLens, person));
+
+console.log('set name:', set(nameLens, 'Marcus', person));
+
+const upper = s => s.toUpperCase();
+console.log('over name:', over(nameLens, upper, person));
