e9f07b8a647043ed6317bd04da49afddd0fa9888
tshemsedinov <timur.shemsedinov@gmail.com>
2019-03-26 15:13:36 +0200

    Add remove field example with lens

--- JavaScript/6-optimize.js
+++ JavaScript/6-optimize.js
@@ -3,10 +3,15 @@
 const view = (lens, obj) => lens.get(obj);
 const set = (lens, val, obj) => lens.set(val, obj);
 const over = (lens, map, obj) => lens.set(map(lens.get(obj)), obj);
+const remove = (lens, obj) => lens.delete(obj);
 
 const lens = (source, destination = source) => ({
   get: obj => obj[source],
-  set: (val, obj) => ({ ...obj, [destination]: val })
+  set: (val, obj) => ({ ...obj, [destination]: val }),
+  delete: obj => {
+    const { [source]: forgot, ...other } = obj;
+    return other;
+  },
 });
 
 // Usage
@@ -19,7 +24,7 @@ const person = {
 
 const upper = s => s.toUpperCase();
 
-// Example 1
+console.log('\nExample 1\n');
 
 const nameLens = lens('name');
 
@@ -27,11 +32,16 @@ console.log('view name:', view(nameLens, person));
 console.log('set name:', set(nameLens, 'Marcus', person));
 console.log('over name:', over(nameLens, upper, person));
 
-// Example 2
-console.log();
+console.log('\nExample 2');
 
 const renameLens = lens('name', 'personName');
 
 console.log('view name:', view(renameLens, person));
 console.log('set name:', set(renameLens, 'Marcus', person));
 console.log('over name:', over(renameLens, upper, person));
+
+console.log('\nExample 3\n');
+
+const exampleLens = lens('name');
+
+console.log('remove name:', remove(exampleLens, person));
